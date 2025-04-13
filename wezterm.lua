fd296b66c09a3521917f4e9d4d84e94d0c422426
Hendrik Mourão <jhendrik@gmail.com>
2025-04-13 15:44:06 -0300

    chore: check os for font

--- wezterm.lua
+++ wezterm.lua
@@ -1,9 +1,17 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
+local is_linux = function()
+	return wezterm.target_triple:find("linux") ~= nil
+end
+
+local is_darwin = function()
+	return wezterm.target_triple:find("darwin") ~= nil
+end
+
 config.color_scheme = "nord"
 config.font = wezterm.font("JetBrains Mono")
-config.font_size = 10.0
+config.font_size = is_darwin() and 14.0 or 10.0
 config.window_background_opacity = 0.95
 config.text_background_opacity = 0.3
 
