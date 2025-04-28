60853da595f9b2634553baeb64612b791e1daf90
Hendrik Mourão <jhendrik@gmail.com>
2025-04-27 23:14:58 -0300

    configure wayland and maxmized

--- wezterm.lua
+++ wezterm.lua
@@ -1,5 +1,6 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
+local mux = wezterm.mux
 
 local is_linux = function()
 	return wezterm.target_triple:find("linux") ~= nil
@@ -9,10 +10,20 @@ local is_darwin = function()
 	return wezterm.target_triple:find("darwin") ~= nil
 end
 
+wezterm.on("gui-startup", function()
+	local tab, pane, window = mux.spawn_window({})
+	window:gui_window():maximize()
+end)
+
 config.color_scheme = "nord"
 config.font = wezterm.font("JetBrains Mono")
 config.font_size = is_darwin() and 14.0 or 10.0
 config.window_background_opacity = 0.95
 config.text_background_opacity = 0.3
+
+if is_linux() then
+	config.enable_wayland = true
+	config.front_end = "WebGpu"
+end
 
 return config
